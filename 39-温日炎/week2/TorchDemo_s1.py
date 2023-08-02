09f2c5c99ed24842e5c12f97b4f3eb85a81e3d2d
Wenriyan1 <18174704765@163.com>
2023-08-03 00:33:57 +0800

    修改作业

--- 39-温日炎/week2/TorchDemo_s1.py
+++ 39-温日炎/week2/TorchDemo_s1.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 如果第1个数>第4个数，则为2类样本，
 如果第1个数>第3个数，则为3类样本，
 如果第1个数>第2个数，则为4类样本
+其它为5类样本
 
 """
 
@@ -24,34 +25,36 @@ class TorchModel(nn.Module):
     def __init__(self, input_size):
         super(TorchModel, self).__init__()
         self.linear = nn.Linear(input_size, 4)  # 线性层
-        self.activation = torch.nn.Softmax # sigmoid归一化函数，激活函数
-        self.loss = nn.CrossEntropyLoss  # loss函数采用均方差损失
+        self.activation = torch.nn.Softmax  # sigmoid归一化函数，激活函数
+        self.loss = nn.functional.cross_entropy  # loss函数采用交叉熵
 
     # 当输入真实标签，返回loss值；无真实标签，返回预测值
     def forward(self, x, y=None):
         x = self.linear(x)  # (batch_size, input_size) -> (batch_size, 1)
         # print(x)
         y_pred = self.activation(x)  # (batch_size, 1) -> (batch_size, 1)
+        # print(y_pred.dim)
         if y is not None:
-            return self.loss(y_pred, y)  # 预测值和真实值计算损失
+            return self.loss(y_pred.dim, y)  # 预测值和真实值计算损失
         else:
             return y_pred  # 输出预测结果
 
 
-# 生成一个样本, 样本的生成方法，代表了我们要学习的规律
-# 随机生成一个5维向量，如果第一个值大于第五个值，认为是正样本，反之为负样本
+# 如果第1个数>第5个数，则为1类样本，
+# 如果第1个数>第4个数，则为2类样本，
+# 如果第1个数>第3个数，则为3类样本，
+# 如果第1个数>第2个数，则为4类样本
+# 其它为5类样本
 def build_sample():
     x = np.random.random(5)
     if x[0] > x[4]:
-        return x, 1
-    elif x[0] > x[3]:
-        return x, 2
-    elif x[0] > x[2]:
-        return x, 3
-    elif x[0] > x[2]:
-        return x, 4
-    else:
         return x, 0
+    elif x[0] > x[3]:
+        return x, 1
+    elif x[0] > x[2]:
+        return x, 2
+    else:
+        return x, 3
 
 
 # 随机生成一批样本
@@ -62,8 +65,8 @@ def build_dataset(total_sample_num):
     for i in range(total_sample_num):
         x, y = build_sample()
         X.append(x)
-        Y.append([y])
-    return torch.FloatTensor(X), torch.FloatTensor(Y)
+        Y.append(y)
+    return torch.FloatTensor(X), torch.LongTensor(Y)
 
 
 # 测试代码
@@ -72,51 +75,54 @@ def evaluate(model):
     model.eval()
     test_sample_num = 100
     x, y = build_dataset(test_sample_num)
+    print("一类样本：%d,二类样本：%d,三类样本：%d，四类样本：%d" % (sum(y.eq(0)), sum(y.eq(1)), sum(y.eq(2)), sum(y.eq(3))))
     # print("本次预测集中共有%d个正样本，%d个负样本" % (sum(y), test_sample_num - sum(y)))
-    type1, type2, type3, type4, wrong = 0, 0, 0, 0, 0
+    # type1, type2, type3, type4, type5, worng = 0, 0, 0, 0, 0, 0
+    correct, wrong = 0, 0
     with torch.no_grad():
         y_pred = model(x)  # 模型预测
-        # print(y_pred)
-
-
-
-
-
+        # print(y_pred.dim)
+        y_pred = y_pred.dim
+        for y_p, y_t in zip(torch.tensor(y_pred), y):
+            if y_t == y_p.argmax():
+                correct += 1;
+            else:
+                wrong += 1
+        return correct/(correct+wrong)
 
 
 def main():
     # 配置参数
-    epoch_num = 1000  # 训练轮数
-    batch_size = 50000  # 每次训练样本个数
-    train_sample = 20  # 每轮训练总共训练的样本总数
+    epoch_num = 10000  # 训练轮数
+    batch_size = 20  # 每次训练样本个数
+    train_sample = 1000  # 每轮训练总共训练的样本总数
     input_size = 5  # 输入向量维度
-    learning_rate = 0.0001  # 学习率
+    learning_rate = 0.1  # 学习率
     # 建立模型
     model = TorchModel(input_size)
     # print(model)
     # 选择优化器
     optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
     log = []
-    # 创建训练集，正常任务是读取训练集
-    train_x, train_y = build_dataset(train_sample)
+
     # print(train_x, train_y)
     # 训练过程
     for epoch in range(epoch_num):
         model.train()
         watch_loss = []
-        for batch_index in range(train_sample // batch_size):
-            x = train_x[batch_index * batch_size: (batch_index + 1) * batch_size]
-            y = train_y[batch_index * batch_size: (batch_index + 1) * batch_size]
-            loss = model(x, y)  # 计算loss
-            loss.backward()  # 计算梯度
-            if batch_index % 2 == 0:
-                optim.step()  # 更新权重
-                optim.zero_grad()  # 梯度归零
-            watch_loss.append(loss.item())
-        # print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
-        acc = evaluate(model)  # 测试本轮模型结果
-        log.append([acc, float(np.mean(watch_loss))])
+        # 创建训练集，正常任务是读取训练集
+        train_x, train_y = build_dataset(train_sample)
+        # print(train_x, train_y)
+        loss = model(train_x, train_y)  # 计算loss
+        loss.backward()  # 计算梯度
+        optim.step()  # 更新权重
+        optim.zero_grad()  # 梯度归零
+        watch_loss.append(loss)
+        # log.append([acc, float(np.mean(watch_loss))])
     # 保存模型
+    acc = evaluate(model)  # 测试本轮模型结果
+    # print("loss%d", watch_loss)
+    print("acc%d", acc)
     torch.save(model.state_dict(), "model2.pth")
     # 画图
     # print(log)
