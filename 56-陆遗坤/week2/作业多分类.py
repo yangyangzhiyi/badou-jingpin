0b79fa52a6b1f06406f81f8939a352947ef01c39
questerMan <luyikun01@163.com>
2023-08-10 00:04:11 +0800

    Add files via upload

--- 56-陆遗坤/week2/作业多分类.py
+++ 56-陆遗坤/week2/作业多分类.py
@@ -4,18 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 输入一个10纬度向量，第1个数大于从左第二开始算起第几个数就是第几分类，如果都不大于属于0分类：以0下标开始计数
-# 将输入转化为onehot矩阵
-def to_one_hot(target, shape):
-    one_hot_target = np.zeros(shape)
-    for i, t in enumerate(target):
-        one_hot_target[i][t] = 1
-    return one_hot_target
 class TorchModel(nn.Module):
     def __init__(self, input_size):
         super().__init__()
-        self.linear = nn.Linear(input_size, 10)
+        size = 128
+        self.linear1 = nn.Linear(input_size, size)
+        self.ac = torch.relu
+        self.linear = nn.Linear(size, 10)
         self.loss = nn.CrossEntropyLoss()
     def forward(self, x, y=None):
+        x = self.linear1(x)
+        x = self.ac(x)
         y_pred = self.linear(x)
         if y is not None:
             return self.loss(y_pred, y)
@@ -36,13 +35,14 @@ def build_datas(total_sample_num):
     for i in range(total_sample_num):
         x, y = build_sample()
         X.append(x)
-        Y.append([y])
-    return torch.FloatTensor(np.array(X)), torch.FloatTensor(np.array(Y))
+        Y.append(y)
+    return torch.FloatTensor(X), torch.LongTensor(Y)
+
 
 def main():
     epoch_num = 10
     batch_size = 20
-    train_sample = 5000
+    train_sample = 50000
     input_size = 10      # 输入维度
     learning_rate = 0.01
     # 建立模型
@@ -60,7 +60,7 @@ def main():
             x = train_x[index*batch_size:(index+1)*batch_size]
             y = train_y[index * batch_size:(index + 1) * batch_size:]
             # 计算loss
-            loss = model(x,y)
+            loss = model(x, y)
             # 计算梯度
             loss.backward()
             # 更新权重
@@ -76,10 +76,10 @@ def main():
     torch.save(model.state_dict(),"modelt.pth")
     # 画图
     print(log)
-    # plt.plot(range(len(log)), [l[0] for l in log], label="acc")  # 画acc曲线
-    # plt.plot(range(len(log)), [l[1] for l in log], label="loss")  # 画loss曲线
-    # plt.legend()
-    # plt.show()
+    plt.plot(range(len(log)), [l[0] for l in log], label="acc")  # 画acc曲线
+    plt.plot(range(len(log)), [l[1] for l in log], label="loss")  # 画loss曲线
+    plt.legend()
+    plt.show()
     return
 
 # 计算准确率
@@ -87,13 +87,15 @@ def evaluate(model):
     model.eval()
     test_sample_num = 100
     x, y = build_datas(test_sample_num)
-    print("本次样本总数%d: 正样本有%d,副样本有%d"%(test_sample_num, sum(y), test_sample_num - sum(y)))
+    # print("本次样本总数%d: 正样本有%d,副样本有%d"%(test_sample_num, sum(y), test_sample_num - sum(y)))
     correct, wrong = 0, 0
     # 忽略梯度
     with torch.no_grad():
         pred_y = model(x)
         for y_p, y_t in zip(pred_y, y):
-            if y_p == y:
+            y_p = y_p.tolist()
+            y_t = y_t.tolist()
+            if y_p.index(max(y_p)) == y_t:
                 correct += 1
             else:
                 wrong += 1
@@ -118,6 +120,7 @@ def predict(model_path, input_vec):
         result = model.forward(torch.FloatTensor(input_vec))
         for vec, res in zip(input_vec, result):
             # 打印结果
+            print(result)
             print("输入：%s, 预测类别：%d， 概率值: %f"%(vec, round(float(res)), res))
 
 if __name__ == "__main__":
@@ -125,5 +128,9 @@ if __name__ == "__main__":
     test_vec = [[0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],
                 [0.94963533, 0.9524256, 0.95758807, 0.95520434, 0.99890681,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],
                 [0.78797868, 0.67482528, 0.13625847, 0.34675372, 0.09871392,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],
+                [0.89349776, 0.99416669, 0.92579291, 0.41567412, 0.7358894,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],[0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],
+                [0.94963533, 0.9524256, 0.95758807, 0.95520434, 0.99890681,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],
+                [0.78797868, 0.67482528, 0.13625847, 0.34675372, 0.09871392,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843],
                 [0.89349776, 0.99416669, 0.92579291, 0.41567412, 0.7358894,0.47889086, 0.15229675, 0.31082123, 0.03504317, 0.18920843]]
     predict("modelt.pth", test_vec)
+
