d59a39b68259534454536533f026ad557aaeccaa
52--李铠 <77481135+techilk@users.noreply.github.com>
2023-08-13 08:30:58 +0800

    Add files via upload
    week4

--- 52-李铠/TorchDemo.py
+++ 52-李铠/TorchDemo.py
@@ -1,147 +1,83 @@
-# coding:utf8
+import jieba
 
-import torch
-import torch.nn as nn
-import numpy as np
-import random
-import json
-import matplotlib.pyplot as plt
-
-"""
-
-基于pytorch框架编写模型训练
-实现一个自行构造的找规律(机器学习)任务
-规律：x是一个5维向量，如果第1个数>第5个数，则为正样本，反之为负样本
-
-"""
-
-
-class TorchModel(nn.Module):
-    def __init__(self, input_size):
-        super(TorchModel, self).__init__()
-        self.linear = nn.Linear(input_size, 1)  # 线性层
-        self.activation = torch.sigmoid  # sigmoid归一化函数
-        self.loss = nn.functional.mse_loss  # loss函数采用均方差损失
-
-    # 当输入真实标签，返回loss值；无真实标签，返回预测值
-    def forward(self, x, y=None):
-        x = self.linear(x)  # (batch_size, input_size) -> (batch_size, 1)
-        y_pred = self.activation(x)  # (batch_size, 1) -> (batch_size, 1)
-        if y is not None:
-            return self.loss(y_pred, y)  # 预测值和真实值计算损失
-        else:
-            return y_pred  # 输出预测结果
+#词典，每个词后方存储的是其词频，仅为示例，也可自行添加
+Dict = {"经常":0.1,
+        "经":0.05,
+        "有":0.1,
+        "常":0.001,
+        "有意见":0.1,
+        "歧":0.001,
+        "意见":0.2,
+        "分歧":0.2,
+        "见":0.05,
+        "意":0.05,
+        "见分歧":0.05,
+        "分":0.1}
 
 
-# 生成一个样本, 样本的生成方法，代表了我们要学习的规律
-# 随机生成一个5维向量，如果第一个值大于第五个值，认为是正样本，反之为负样本
-def build_sample():
-    x = np.random.random(5)
-    if x[0] > x[4]:
-        return x, 1
-    else:
-        return x, 0
+#根据上方词典，对于输入文本，构造一个存储有所有切分方式的信息字典
+#学术叫法为有向无环图，DAG（Directed Acyclic Graph），不理解也不用纠结，只当是个专属名词就好
+#这段代码直接来自于jieba分词
+def calc_dag(sentence):
+        DAG = {}    #DAG空字典，用来存储DAG有向无环图
+        N = len(sentence)
+        for k in range(N):
+            tmplist = []
+            i = k
+            frag = sentence[k]
+            while i < N:
+                if frag in Dict:
+                    tmplist.append(i)
+                i += 1
+                frag = sentence[k:i + 1]
+            if not tmplist:
+                tmplist.append(k)
+            DAG[k] = tmplist
+        return DAG
+
+sentence = "经常有意见分歧"
+print(calc_dag(sentence))
+#结果应该为{0: [0, 1], 1: [1], 2: [2, 4], 3: [3, 4], 4: [4, 6], 5: [5, 6], 6: [6]}
+#0:[0,1]代表句子中的第0个字，可以单独成词，或与第1个字一起成词
+#2:[2,4]代表句子中的第2个字，可以单独成词，或第2-4个字一起成词
+#依次类推
+#这个字典中实际上就存储了所有可能的切分方式的信息
 
 
-# 随机生成一批样本
-# 正负样本均匀生成
-def build_dataset(total_sample_num):
-    X = []
-    Y = []
-    for i in range(total_sample_num):
-        x1= []
-        y1 =[]
-        for j in range(total_sample_num):
-            x, y = build_sample()
-            x1.append(x)
-            y1.append([y])
-        X.append(x1)
-        Y.append(y1)
-    return torch.FloatTensor(X), torch.FloatTensor(Y)
+#将DAG中的信息解码（还原）出来，用文本展示出所有切分方式
+class DAGDecode:
+    #通过两个队列来实现
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.DAG = calc_dag(sentence)  #使用了上方的函数
+        self.length = len(sentence)
+        self.unfinish_path = [[]]   #保存待解码序列的队列
+        self.finish_path = []  #保存解码完成的序列的队列
+
+    #对于每一个序列，检查是否需要继续解码
+    #不需要继续解码的，放入解码完成队列
+    #需要继续解码的，将生成的新队列，放入待解码队列
+    #path形如:["经常", "有", "意见"]
+    def decode_next(self, path):
+        path_length = len("".join(path))
+        if path_length == self.length:  #已完成解码
+            self.finish_path.append(path)
+            return
+        candidates = self.DAG[path_length]
+        new_paths = []
+        for candidate in candidates:
+            new_paths.append(path + [self.sentence[path_length:candidate+1]])
+        self.unfinish_path += new_paths  #放入待解码对列
+        return
+
+    #递归调用序列解码过程
+    def decode(self):
+        while self.unfinish_path != []:
+            path = self.unfinish_path.pop() #从待解码队列中取出一个序列
+            self.decode_next(path)     #使用该序列进行解码
 
 
-# 测试代码
-# 用来测试每轮模型的准确率
-def evaluate(model,length):
-    model.eval()
-    test_sample_num = 100
-    x, y = build_dataset(test_sample_num)
-    #print("本次预测集中共有%d个正样本，%d个负样本" % (sum(y), test_sample_num - sum(y)))
-    correct, wrong = 0, 0
-    with torch.no_grad():
-        for i in range(length):
-            y_pred = model(x[i])  # 模型预测
-            for i in range(length):
-                for y_p, y_t in zip(y_pred[i], y[i]):  # 与真实标签进行对比
-                    if float(y_p) < 0.5 and int(y_t) == 0:
-                        correct += 1  # 负样本判断正确
-                    elif float(y_p) >= 0.5 and int(y_t) == 1:
-                        correct += 1  # 正样本判断正确
-                    else:
-                        wrong += 1
-    print("正确预测个数：%d, 正确率：%f" % (correct, correct / (correct + wrong)))
-    return correct / (correct + wrong)
-
-
-def main():
-    # 配置参数
-    epoch_num = 10  # 训练轮数
-    batch_size = 20  # 每次训练样本个数
-    train_sample = 5000  # 每轮训练总共训练的样本总数
-    input_size = 5  # 输入向量维度
-    learning_rate = 0.001  # 学习率
-    # 建立模型
-    model = TorchModel(input_size)
-    # 选择优化器
-    optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    log = []
-    # 创建训练集，正常任务是读取训练集
-    train_x, train_y = build_dataset(train_sample)
-    # 训练过程
-    for epoch in range(epoch_num):
-        model.train()
-        watch_loss = []
-        for batch_index in range(train_sample // batch_size):
-            x = train_x[batch_index * batch_size : (batch_index + 1) * batch_size]
-            y = train_y[batch_index * batch_size : (batch_index + 1) * batch_size]
-            loss = model(x, y)  # 计算loss
-            loss.backward()  # 计算梯度
-            if batch_index % 2 == 0:
-                optim.step()  # 更新权重
-                optim.zero_grad()  # 梯度归零
-            watch_loss.append(loss.item())
-        print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
-        acc = evaluate(model,batch_size)  # 测试本轮模型结果
-        log.append([acc, float(np.mean(watch_loss))])
-    # 保存模型
-    torch.save(model.state_dict(), "model.pth")
-    # 画图
-    print(log)
-    plt.plot(range(len(log)), [l[0] for l in log], label="acc")  # 画acc曲线
-    plt.plot(range(len(log)), [l[1] for l in log], label="loss")  # 画loss曲线
-    plt.legend()
-    plt.show()
-    return
-
-
-# 使用训练好的模型做预测
-def predict(model_path, input_vec):
-    input_size = 5
-    model = TorchModel(input_size)
-    model.load_state_dict(torch.load(model_path))  # 加载训练好的权重
-    print(model.state_dict())
-
-    model.eval()  # 测试模式
-    with torch.no_grad():  # 不计算梯度
-        result = model.forward(torch.FloatTensor(input_vec))  # 模型预测
-    for vec, res in zip(input_vec, result):
-        print("输入：%s, 预测类别：%d, 概率值：%f" % (vec, round(float(res)), res))  # 打印结果
-
-
-if __name__ == "__main__":
-    main()
-    test_vec = [[0.47889086,0.15229675,0.31082123,0.03504317,0.18920843],
-                [0.94963533,0.5524256,0.95758807,0.95520434,0.84890681],
-                [0.78797868,0.67482528,0.13625847,0.34675372,0.09871392],
-                [0.89349776,0.59416669,0.92579291,0.41567412,0.7358894]]
-    predict("model.pth", test_vec)
+sentence = "经常有意见分歧"
+dd = DAGDecode(sentence)
+dd.decode()
+print(dd.finish_path)
