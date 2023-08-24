622abc1c701a719c6f75bf238d6c98737ac206ae
bupengju <1143170860@qq.com>
2023-08-24 22:26:16 +0800

    week2 练习

--- 134-卜朋举/week02/mlp.py
+++ 134-卜朋举/week02/mlp.py
@@ -3,10 +3,10 @@ import torch
 import torch.nn as nn
 
 
-class TorchLinearReg(nn.Module):
+class TorchMLP(nn.Module):
 
     def __init__(self, input_dim: int, hidden_dim: int, output_dim: int):
-        super(TorchLinearReg, self).__init__()
+        super(TorchMLP, self).__init__()
         self.linear1 = nn.Linear(input_dim, hidden_dim)
         self.linear2 = nn.Linear(hidden_dim, output_dim)
 
@@ -16,7 +16,7 @@ class TorchLinearReg(nn.Module):
         return out
 
 
-class MyLinearReg(object):
+class MyMLP(object):
 
     def __init__(self, w1, w2, b1, b2):
         self.w1 = w1
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     np.random.seed(2023)
     features = np.random.random([2, 3]).astype("f4")  # torch默认支持float32输入
     feat_tensor = torch.from_numpy(features)
-    torch_linear = TorchLinearReg(3, 5, 2)
+    torch_linear = TorchMLP(3, 5, 2)
     torch_pred = torch_linear.forward(feat_tensor)
     print(torch_pred.data)
     print("=" * 50)
@@ -45,6 +45,6 @@ if __name__ == "__main__":
     b1 = state_dict.get("linear1.bias").numpy()
     b2 = state_dict.get("linear2.bias").numpy()
 
-    my_linear = MyLinearReg(w1, w2, b1, b2)
+    my_linear = MyMLP(w1, w2, b1, b2)
     my_pred = my_linear.forward(features)
     print(my_pred)
