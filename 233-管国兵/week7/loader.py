902a65724be2b347131b08fe4a39e7bf618f7db7
Administrator <binglord@hotmail.com>
2023-08-30 19:35:00 +0800

    20230830193456

--- 233-管国兵/week7/loader.py
+++ 233-管国兵/week7/loader.py
@@ -7,14 +7,14 @@
 from config import Config
 import csv
 
-
-with open(Config['train_data_path'], "r", encoding='utf-8') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader) # 跳过第一行标题
-    data = tuple()
-    for row in csv_reader:
-        print(row)
-        data[row(0)] = row[1]
+def load_data():
+    data = []
+    with open(Config['train_data_path'], "r", encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader) # 跳过第一行标题
+        for row in csv_reader:
+            data.append(row)
+    return data
 
 # class DataGenerator:
 #     def __init__(self, config):
