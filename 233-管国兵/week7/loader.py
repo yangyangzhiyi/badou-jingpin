873641c15ec8ed994f99e0930ab62e267f41ee11
Administrator <binglord@hotmail.com>
2023-08-31 18:19:31 +0800

    20230831181926

--- 233-管国兵/week7/loader.py
+++ 233-管国兵/week7/loader.py
@@ -3,23 +3,29 @@
 """
 加载数据
 """
+import random
 
 from config import Config
 import csv
 
-def load_data():
+
+def load_data(train_data_path):
     data = []
-    with open(Config['train_data_path'], "r", encoding='utf-8') as csv_file:
+    with open(train_data_path, "r", encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
-        next(csv_reader) # 跳过第一行标题
+        next(csv_reader)  # 跳过第一行标题
         for row in csv_reader:
             data.append(row)
     return data
 
-# class DataGenerator:
-#     def __init__(self, config):
-#         self.config = config
-#         self.train_data = self.load_data(config.train_data_path)
-#
-#     def load_data(self, data_path):
-#         data = []
+
+def random_choice_data(data, num):
+    random_data = []
+    for i in range(num):
+        random_data.append(random.choice(data))
+    return random_data
+
+
+data = load_data(Config['train_data_path'])
+random_data = random_choice_data(data, 1000)
+print(random_data)
