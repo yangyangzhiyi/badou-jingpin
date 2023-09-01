7f3e7f6aa5249fad7a4781ad2cf1cbbd9dd659c4
Jaie <jaie@zbyte.com.cn>
2023-09-01 21:44:32 +0800

    week7

--- 130-彭积文/week7/config.py
+++ 130-彭积文/week7/config.py
@@ -5,6 +5,7 @@ Config = {
     "device": "cuda" if torch.cuda.is_available() else "cpu",
     "input_dim": 768,
     "hidden_dim": 1024,
+    "lstm_layers": 2,
     "output_dim": 2,
     "batch_size": 300,
     "senence_max_length": 512,
--- 130-彭积文/week7/model.py
+++ 130-彭积文/week7/model.py
@@ -8,18 +8,14 @@ class BertModule(nn.Module):
         super(BertModule,self).__init__()
         self.__config = config
         self.__bert = BertModel.from_pretrained("../bert", return_dict=False).to(self.__config["device"])
-        self.__linear1 = nn.Linear(self.__config["input_dim"], self.__config["hidden_dim"]).to(self.__config["device"])
-        self.__activation = nn.Sigmoid()
-        self.__linear2 = nn.Linear(self.__config["hidden_dim"], self.__config["output_dim"]).to(self.__config["device"])
-        self.__dropout = nn.Dropout(0.2)
-        self.__loss = nn.CrossEntropyLoss(ignore_index=-100)
+        self.__lstm = nn.LSTM(self.__config["input_dim"], self.__config["input_dim"], num_layers=self.__config["lstm_layers"], batch_first=True).to(self.__config["device"])
+        self.__linear = nn.Linear(self.__config["input_dim"], self.__config["output_dim"]).to(self.__config["device"])
+        self.__loss = nn.CrossEntropyLoss()
 
     def forward(self,x,y=None):
         _,x = self.__bert(x)
-        x = self.__linear1(x)
-        x = self.__activation(x)
-        x = self.__linear2(x)
-        y_pred = self.__dropout(x)
+        x,_ = self.__lstm(x)
+        y_pred = self.__linear(x)
 
         if y is not None:
             return self.__loss(y_pred, y.squeeze())
